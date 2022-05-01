a7761d6962130b2ef5b054d028f6bd09473eea54
Harsh Singh <64768386+harshsinghcodes@users.noreply.github.com>
2022-05-01 19:44:52 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
-const operators = ["+","-","/","*","%","<",">","&&",">=","<=","==", "===","||","!=","!==","=","+=","-="];
+const operators = ["+","-","/","*","%","<",">","&&",">=","<=","==", "===","||","!=","!==","=","+=","-=","/=","*="];
 
-const keywords = "let|each|array|func|do|eldo|if|while".split("|");
+const keywords = "let|loop|map|array|defun|do|eldo|if|while|return".split("|");
 
 const isOperator = v => operators.indexOf(v) >= 0;
 
